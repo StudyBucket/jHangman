1fc85071e20ee5d97331ad2f4be4690a4a78770b
Julian Kamphausen <juliankamphausen@me.com>
2020-12-19 16:23:56 +0100

    separate WordIO from WordStorage

--- jHangman/src/JHangman.java
+++ jHangman/src/JHangman.java
@@ -1,9 +1,11 @@
+import words.WordStorage;
 
 public class JHangman {
 
 	public static void main(String[] args) {
 		WordStorage wordStorage = new WordStorage();
-		wordStorage.loadWords("tech.txt");
+		wordStorage.loadWords("tech");
+		
 		System.out.println(wordStorage.getRandomWord());
 		System.out.println(wordStorage.getRandomWord());
 		System.out.println(wordStorage.getRandomWord());
--- jHangman/src/words/WordStorage.java
+++ jHangman/src/words/WordStorage.java
@@ -1,47 +1,66 @@
-import java.io.File;
+package words;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class WordStorage {
 	
-	private static final String BASE_PATH = "./src/resources/";
-	boolean filesLoaded = false;
+	boolean verboseMode;
+	String currentFileName = null;
 	List<String> wordList = new ArrayList<String>();
+	WordIO io = new WordIO();
 	
+	public WordStorage(boolean verboseMode){
+		this.verboseMode = verboseMode;
+	}
 	
 	public WordStorage(){
-		
+		this.verboseMode = false;
+	}
+	
+	public void addWord(String word) {
+		int tempSize = this.wordList.size();
+		this.wordList.add(word);
+		this.verboseMessage("[WordStorage] '" + word + "' added to current word list. List consists of " + this.wordList.size() + " words. (previously " + tempSize + " words)");
 	}
 	
 	public String getRandomWord() {
-		int randomNum = ThreadLocalRandom.current().nextInt(0, this.wordList.size());
-		return this.wordList.get(randomNum);
+		if(this.currentFileName != null) {
+			int randomNum = ThreadLocalRandom.current().nextInt(0, this.wordList.size());
+			return this.wordList.get(randomNum);
+		}
+		return null;
 	}
 	
 	public void loadWords(String fileName) {
-		this.readWordsFromFileToArray(fileName);
+		this.verboseMessage("[WordStorage] Loading words from '" + fileName + "' ...");
+		this.currentFileName = fileName;
+		this.wordList = io.loadWordFile(fileName);
+		this.verboseMessage("[WordStorage] " + this.wordList.size() + " word loaded.");
 	}
 	
-	public void resetWords() {
+	public void discardChanges() {
+		this.loadWords(currentFileName);
+		this.verboseMessage("[WordStorage] Discarded changes to current word list.");
+	}
+		
+	public void saveChanges() {
+		this.io.saveWordFile(this.currentFileName, this.wordList);
+		this.verboseMessage("[WordStorage] Changes to current word list saved.");
+	}
+	
+	public void unloadWords() {
+		this.currentFileName = null;
 		this.wordList = new ArrayList<String>();
+		this.verboseMessage("[WordStorage] Current word list unloaded.");
 	}
 	
 	public int count() {
 		return this.wordList.size();
 	}
 	
-	private void readWordsFromFileToArray(String fileName) {
-		try {
-			File file = new File(BASE_PATH + fileName);
-			Scanner input = new Scanner(file);
-			while (input.hasNextLine()) {
-				wordList.add(input.nextLine());
-			}
-			input.close();
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}	
+	private void verboseMessage(String message) {
+		if(this.verboseMode) System.out.println(message);
+	}
 }
